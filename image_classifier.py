c2912b3a8d9ff19f22ef5d68bf8ff08d8ab9a038
rupakkarki27 <rupakkarki123@gmail.com>
2020-10-10 10:00:10 +0545

    fixed indentation errors

--- image_classifier.py
+++ image_classifier.py
@@ -16,17 +16,17 @@ def process_image(img, img_size=(224, 224)):
   Returns:
     An Image array that is ready to be fed into the model.
   """
-    # reshapes the image
-    image = ImageOps.fit(img, img_size, Image.ANTIALIAS)
-    # converts the image into numpy array
-    image = np.asarray(image)
-    # converts image from BGR color space to RGB
-    img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    img_resize = (cv2.resize(img, dsize=img_size, interpolation=cv2.INTER_CUBIC))/255.
+  # reshapes the image
+  image = ImageOps.fit(img, img_size, Image.ANTIALIAS)
+  # converts the image into numpy array
+  image = np.asarray(image)
+  # converts image from BGR color space to RGB
+  img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+  img_resize = (cv2.resize(img, dsize=img_size, interpolation=cv2.INTER_CUBIC))/255.
 
-    img_reshape = img_resize[np.newaxis,...]
-    
-    return img_reshape
+  img_reshape = img_resize[np.newaxis,...]
+  
+  return img_reshape
 
 
 def prediction_result(model, image_data):
@@ -40,18 +40,18 @@ def prediction_result(model, image_data):
 
   """
   # Mapping prediction results to the Flower type
-    classes = {0: "Daisy", 
-               1: "Dandelion",
-               2: "Rose",
-               3: "Sunflower",
-               4: "Tulip"}
-    
-    pred = model.predict(image_data)
-    pred = pred.round(2)
-    result = np.argmax(pred)
-    
-    prediction = {"class": classes[result],
-                  "accuracy": np.round(np.max(pred) * 100, 2)}
-    
-    return prediction
+  classes = {0: "Daisy", 
+             1: "Dandelion",
+             2: "Rose",
+             3: "Sunflower",
+             4: "Tulip"}
+  
+  pred = model.predict(image_data)
+  pred = pred.round(2)
+  result = np.argmax(pred)
+  
+  prediction = {"class": classes[result],
+                "accuracy": np.round(np.max(pred) * 100, 2)}
+  
+  return prediction
 
